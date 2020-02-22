917f8c4ea313f2cc3d203cdbbe7940937fb148a2
Mega Gnome <mateuszlk1998@gmail.com>
2020-02-22 19:33:59 +0000

    Added post-processing

---
+++ Assets/Scripts/Dayscale.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dayscale : MonoBehaviour
+{
+    [Tooltip("number of minutes per second")]
+    public float minutesPerSecond;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        float angleThisFrame = Time.deltaTime / 360*minutesPerSecond
+    }
+}
